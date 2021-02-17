91fb170eff89d423693e3a0e788badec46eef65d
Kiana Long <kianajade42.kl@gmail.com>
2021-02-17 18:42:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,16 +7,15 @@ def initialize(name,grade,id=nil)
   @name=name
   @grade=grade
 end
-
 def self.create_table
-  sql=<<-SQL
-  CREATE TABLE IF NOT EXISTS students
-(
-id INTEGER PRIMARY KEY
-name TEXT
-grade TEXT
-    )
-    SQL DB[:conn].execute(sql)
+  sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+  SQL
+  DB[:conn].execute(sql)
 end
 
 def self.drop_table
